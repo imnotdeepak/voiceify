3db893b0742bfa7093912763cdc8fb402366f6da
Lychee 🖤 <rambutanfruitjuice@gmail.com>
2026-03-31 13:10:59 -0400

    Created the dashboard UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
           {children}
           <Toaster />
